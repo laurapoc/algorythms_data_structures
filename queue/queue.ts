cd25b2b4055b72559adbdb49860950c1f0902c99
laurapoc <laura.pociuniene@gmail.com>
2022-10-19 12:07:54 +0300

    Solve firstUniqChar task from leetcode.com

--- queue/queue.ts
+++ queue/queue.ts
@@ -1,3 +1,37 @@
-const queueArr: number[] = new Array(5);
+// First Unique Character in a String:
+// Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
 
-console.log(queueArr);
+// Example 1:
+
+// Input: s = "leetcode"
+// Output: 0
+// Example 2:
+
+// Input: s = "loveleetcode"
+// Output: 2
+// Example 3:
+
+// Input: s = "aabb"
+// Output: -1
+
+// Constraints:
+
+// 1 <= s.length <= 105
+// s consists of only lowercase English letters.
+
+const firstUniqChar: number | any = (s: string) => {
+  const chartsArr = s.split("");
+  const uniqChart = chartsArr.find(
+    (x: string) => chartsArr.indexOf(x) === chartsArr.lastIndexOf(x)
+  );
+  for (const char of chartsArr) {
+    if (char === uniqChart) {
+      return chartsArr.indexOf(char);
+    }
+  }
+  return -1;
+};
+
+console.log(firstUniqChar("loveleetcode"));
+console.log(firstUniqChar("leetcode"));
+console.log(firstUniqChar("aabb"));
